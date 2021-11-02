e2982fdf7eaf41a3573c769d88dc98fb473e4a66
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-11-02 15:04:47 -0300

    Fix errors in todo-list.js

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,20 +3,23 @@ import TodoItem from './todo-item.js'; // eslint-disable-line no-unused-vars
 import './todo-list.css';
 
 const TodoList = (props) => {
+
+  function outputItems(item, index) {
+    return (
+      <TodoItem
+        checkboxHandler={props.checkboxHandler}
+        removeItem={props.removeItem}
+        key={index.toString()}
+        item={item}
+        index={index}
+      ></TodoItem>
+    );
+  }
+
   if (props.items.length !== 0) {
     return (
       <ul>
-        {props.items.map(function (item, index) {
-          return (
-            <TodoItem
-              checkboxHandler={props.checkboxHandler}
-              removeItem={props.removeItem}
-              key={index.toString()}
-              item={item}
-              index={index}
-            ></TodoItem>
-          );
-        })}
+        {props.items.map((item, index) => outputItems(item, index))}
       </ul>
     );
   } else { // eslint-disable-line no-else-return
