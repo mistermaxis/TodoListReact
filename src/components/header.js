d042e08f3fa553602033261f47ed04f849745998
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-11-02 14:59:12 -0300

    Fix linter error in header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 
-const Header = () => {
+function Header() {
   return (
     <header className="header">
       <h1 className="header-title">Todo List</h1>
