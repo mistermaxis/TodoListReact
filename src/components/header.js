9ae09494ce8e6674492a9ce384a9fdb0f04d414e
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-11-02 14:41:38 -0300

    Fix linter errors in header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 
 const Header = () => {
-    return (
-        <header className="header">
-            <h1 className="header-title">Todo List</h1>
-        </header>
-    );
-}
- 
+  return (
+    <header className="header">
+      <h1 className="header-title">Todo List</h1>
+    </header>
+  );
+};
+
 export default Header;
